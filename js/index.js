375bd1f51bad9ad006674e58ccffc029c58371fa
tugi-ngunjiri <blueqwinn@gmail.com>
2022-12-06 21:21:16 +0300

    done with my assigment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {});
+ //Like <button 
+let like="11";
+
+increaseLike = ()  => {
+ like ++ ,
+ getLikeNum.inner.HTML = `${like}`
+}
+
+likeClick, (11) ; {
+increaseLike(11)
+
+}
+
+getLike.addEventListener(('click'), likeClick)
